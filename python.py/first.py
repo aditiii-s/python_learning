8a8ffb14b968d50101b4275a585abe15fbed86e5
aditii-s <mycollegeworkk@gmail.com>
2026-02-27 14:45:49 +0530

    first python commit

---
+++ python.py/first.py
@@ -0,0 +1,9 @@
+print('aditi')
+gender ='girls' # we have to define it is  a string #
+#print (gender)#this is about the variables you dont have to put semicolon in it 
+#if we put gender = boys it finally print boy the last one is printed 
+gender = 'boys'
+print (gender)
+#when we take input from the user 
+name=input("who is aditi?")
+print("aditi"+ name) # you can give input as a whole sentence like aditi + "is a girl "
