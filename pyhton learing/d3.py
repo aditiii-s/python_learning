2fdbf6c9048880c9cdd65b6b6596c61a9ed65f44
aditii-s <mycollegeworkk@gmail.com>
2026-03-07 09:21:47 +0530

    day 3 of learning op,find ,replace and len()

---
+++ pyhton learing/d3.py
@@ -0,0 +1,20 @@
+#LEN() IS USED TO COUNT THE NUMBER OF CHARACTERS 
+name="aditi singh"
+print(len(name))#it gives the number of characters 
+#THERE ARE DIFFERENT OP TO CHANGE THE FORM LIKE IN UPPERCASE LOWERCASE FINDING REPLACING 
+print(name.upper())#convert all characters into captial letters 
+print(name.lower())#convert all into small charaacters 
+
+#find and replace is case sensitive 
+print(name.find('i'))#finds nothing return -1 and inside find must be a string to find and returns index of it 
+print(name.replace('aditi','siya'))
+#WRITE FIRST WHICH IS PRESENT AND THE OTHER ONE IS ONE WHICH HAVE TO WRITE AFTER REPLACEMENT 
+
+#IN OPERATOR IS USED TO CHECK THE WORD IS PRESENT IN IT OR NOT 
+print('singh' in name)#it gives true if agar toda bhi space idhar udhar hua toh false de dega
+
+#ARIMETIC OPE
+print(10**3)
+x=3.4
+print(round(x))
+
