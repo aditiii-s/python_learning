33ce0dd9b8fc8f3490b89bfc895dd112a3861fa1
aditii-s <mycollegeworkk@gmail.com>
2026-03-11 17:23:24 +0530

    just for fun

---
+++ pyhton learing/practice.py/p1.py
@@ -0,0 +1,6 @@
+money=int(input("how much?"))
+#I WANT TO PRINT IT OUT MONEY = 20 HOW COULD I ?? ---by using formatted strings 
+print(f"money={money}")
+samaj=input("kaisa hai ")
+print()
+print(f"samaj {samaj}")
