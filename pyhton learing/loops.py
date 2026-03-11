acd36254be827e45eb346cfaa0ccc772f63b597f
aditii-s <mycollegeworkk@gmail.com>
2026-03-11 19:07:25 +0530

    loops upto else

--- pyhton learing/loops.py
+++ pyhton learing/loops.py
@@ -1,1 +1,12 @@
-#HERE WE START WITH IF LOOP
+#HERE WE START WITH IF LOOP AND THEN IT IS CONVERTED INTO ELSE IF AND WE TALK ABOUT ABOUT WHILE LOOP
+hot_day = True #it is case sensitive true is must written with capital T
+cold_day = False 
+if hot_day :
+    print("gye sb")
+elif cold_day:#else if mei condition k sath likhna padta hai bina condition k nhi work krega 
+
+    print('thandhi hai')
+
+else :
+
+    print("thik hi hai")    
