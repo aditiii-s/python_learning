6ea503405df1ca3bbcfe0c7951b665b2906195a1
aditii-s <mycollegeworkk@gmail.com>
2026-03-19 21:14:55 +0530

    car game

--- pyhton learing/practice.py/cargame.py
+++ pyhton learing/practice.py/cargame.py
@@ -1,1 +1,19 @@
 #we have gonnna to make a car game simply for starting 
+command=" "
+while True:
+    command=input(">").lower()
+    if command =="start":
+        print("car started")
+    elif command=="stop":
+        print("car stopped ")
+
+    elif command=="help":
+     print(""" 
+start --- to start the car 
+stop --- to stop the car 
+quit --- to quit                  
+ """)    
+    elif command =="quit":
+        break
+else:
+    print("sorry , dont get it ")        
